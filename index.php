9a78800946ead835ebdd197ed112ae009ee5ab08
Jose guilherme <guilherme.correia13@hotmail.com>
2017-11-15 15:57:53 -0200

    tpl site

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php 
-
+//O vendor/autoload.php é do composer, para trazer as dempendencias
 require_once("vendor/autoload.php");
-
+// os Use são namespace 
 	use \Slim\Slim;
 	use \Hcode\Page;
-
 
 $app = new \Slim\Slim();
 
